d052f8615bbaad3faca040007c5c9f0518660d75
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-01 09:01:45 -0300

    WIP test data

--- src/data/__test__/index.test.js
+++ src/data/__test__/index.test.js
@@ -1,5 +1,8 @@
 /// <reference types="jest" />
 // eslint-disable-next-line import/extensions
+import fixturePokemon from './mockList.json';
+import 'regenerator-runtime/runtime' ; '@babel/runtime'
+
 import { mapearDatosPokemon, mapearListaPokemones } from '../index.js';
 
 beforeEach(() => {
@@ -7,13 +10,20 @@ beforeEach(() => {
 });
 
 test('Carga pokemon', async () => {
+  global.fetch.mockReturnValueOnce(new Promise((resolve) => {
+    const jsonPromise = new Promise((resolve) => resolve(fixturePokemon))
+    resolve(jsonPromise);
+  }))
   await mapearDatosPokemon(2);
   expect(global.fetch).toBeCalledTimes(1);
   expect(global.fetch).toBeCalledWith('https://pokeapi.co/api/v2/pokemon/2');
-})
+});
 
 test('Carga lista Pokemon', async () => {
+    global.fetch.mockReturnValueOnce(new Promise((resolve) => {
+    resolve({json: () => "./mockList .json"});
+  }))
   await mapearListaPokemones(2);
   expect(global.fetch).toBeCalledTimes(1);
   expect(global.fetch).toBeCalledWith('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=10');
-})
+});
