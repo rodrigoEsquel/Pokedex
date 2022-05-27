0edd3c2736de921367fa8459f045df29edfa5411
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-26 23:26:16 -0300

    Fix localStorage ListaPokemon bug

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/extensions
 import { cargarPokemon, cargarListaPokemon } from './api.js';
-import { guardarLocalStorage, buscarLocalStorage } from "./localStorage";
-import Pokemon from '../clases/pokemon.js';
-import ListaPokemon from "../clases/listaPokemon.js";
+import { guardarLocalStorage, buscarLocalStorage } from "./localStorage.js";
+import Pokemon from '../classes/Pokemon.js';
+import ListaPokemon from "../classes/ListaPokemon.js";
 
 export async function mapearDatosPokemon(pokemon) {
   if (buscarLocalStorage('poke-' + pokemon)) {
@@ -15,7 +15,7 @@ export async function mapearDatosPokemon(pokemon) {
 
 export async function mapearListaPokemones(pagina) {
   if (buscarLocalStorage('lista-' + pagina)) {
-    return new Pokemon(buscarLocalStorage('lista-' + pagina));      
+    return new ListaPokemon(pagina, (buscarLocalStorage('lista-' + pagina)));      
   }
   const lista = await cargarListaPokemon(pagina);
   guardarLocalStorage(('lista-' + pagina), lista);
